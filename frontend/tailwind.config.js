a9cedbb2eb572ab442e56b3a20f0c5dd09999541
MikiJRs <emerttd@gmail.com>
2024-10-04 16:45:54 +0300

    folder deleted

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  // Projedeki tüm dosyaları izlemesi için bu ayarı ekliyoruz
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
