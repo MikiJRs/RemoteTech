e6cf7d4c99703728c494a0f7168942426dc596f8
MikiJRs <emerttd@gmail.com>
2024-10-04 16:45:09 +0300

    frontend changed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Projedeki tüm dosyaları izlemesi için bu ayarı ekliyoruz
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
